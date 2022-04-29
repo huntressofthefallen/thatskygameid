2b45dcc9d77e114f20b089f733978ec1139dc1b3
Huntress of the Fallen <andika.bagaskara@gmail.com>
2022-04-29 08:47:55 +0700

    new doncrafting

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.redirect('/SoAbyssConceptArt')
-});
+// router.get('/', function (req, res, next) {
+//   res.render('index');
+// });
 
 router.get('/SoAbyssConceptArt', (req, res, next) => {
   res.render('SoAbyssConceptArtID');
+});
+
+router.get('/DoNCrafting', (req, res, next) => {
+  res.render('DoNCraftingID');
 });
 
 module.exports = router;
