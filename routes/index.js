8391f95d05ade70b746900e09a91bf845fdff2dc
Huntress of the Fallen <andika.bagaskara@gmail.com>
2022-05-18 10:26:29 +0700

    new linktree

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.redirect('https://www.thatskygame.com/');
+  res.render('index');
 });
 
-router.get('/SoAbyssConceptArt', (req, res, next) => {
+router.get('/id/news/the-art-behind-season-of-abyss', (req, res, next) => {
   res.render('SoAbyssConceptArtID');
+});
+
+router.get('SoAbyssConceptArt', (req, res, next) => {
+  res.redirect('/id/news/the-art-behind-season-of-abyss');
 });
 
 router.get('/DoNCrafting', (req, res, next) => {
